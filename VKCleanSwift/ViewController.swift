d1d1897e8dc7b5a4755ab4f583a6c4e7772755d9
Sergey <12kaptan12@gmail.com>
2019-06-17 17:15:21 +0300

    Initial Commit

---
+++ VKCleanSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKCleanSwift
+//
+//  Created by Sergey Koriukin on 17/06/2019.
+//  Copyright © 2019 Sergey Koriukin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
