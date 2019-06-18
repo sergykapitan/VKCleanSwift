b0b08c50aa362772f92c8d0a670a4225f4c7d005
Sergey <12kaptan12@gmail.com>
2019-06-18 14:46:18 +0300

    change API

--- VKCleanSwift/Servise/API.swift
+++ VKCleanSwift/Servise/API.swift
@@ -16,9 +16,4 @@ struct API {
     static let newsFeed = "/method/newsfeed.get"
     
 }
-//https://api.vk.com/method/newsfeed.get?user_ids=187514081&filters=post,photo&access_token=1d57c28c629dceb6c1b5766b7570370e922a951fab72f9639f18cc65780ca7a30407a836c505613b8bffa&v=5.95
 
-// https://api.vk.com/method/users.get?
-// user_ids=210700286&fields=bdate&access_token=533bacf01e11f55b536a565b57531ac114461ae8736d6506a3&v=5.95
-
-//https://api.vk.com/method/users.get?user_ids=187514081&fields=bdate&access_token=1d57c28c629dceb6c1b5766b7570370e922a951fab72f9639f18cc65780ca7a30407a836c505613b8bffa&v=5.95
