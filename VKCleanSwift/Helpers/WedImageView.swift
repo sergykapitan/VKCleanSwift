b0eaea14b8257a17aa102c38aab1dcce7348f91f
Sergey <12kaptan12@gmail.com>
2019-09-04 10:02:30 +0300

    add Caching Image

--- VKCleanSwift/Helpers/WedImageView.swift
+++ VKCleanSwift/Helpers/WedImageView.swift
@@ -12,13 +12,25 @@ class WebImageView: UIImageView {
     
     func set(imageUrl: String) {
         guard let url = URL(string: imageUrl) else { return }
+        if let cachedResponse = URLCache.shared.cachedResponse(for: URLRequest(url: url)) {
+            self.image = UIImage(data: cachedResponse.data)
+            print("from cache")
+            return
+        }
+            print("from inet")
         let dataTask = URLSession.shared.dataTask(with: url) { [weak self](data, response, error) in
             DispatchQueue.main.async {
-                if let data = data {
+                if let data = data, let response = response {
                     self?.image = UIImage(data: data)
+                    self?.handleLoadedImage(data: data, response: response)
                 }
             }
         }
         dataTask.resume()
+    }
+    private func handleLoadedImage(data: Data, response: URLResponse) {
+        guard let responseURL = response.url else { return }
+        let cachedResponse = CachedURLResponse(response: response, data: data)
+        URLCache.shared.storeCachedResponse(cachedResponse, for: URLRequest(url: responseURL))
     }
 }
