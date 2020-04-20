ebaca8b817695e8dbe98b35393b48d547f61dba5
Sergey <12kaptan12@gmail.com>
2020-04-20 15:42:29 +0300

    Adding Neumorph style

--- VKCleanSwift/Auth/AuthViewController.swift
+++ VKCleanSwift/Auth/AuthViewController.swift
@@ -12,27 +12,53 @@ class AuthViewController: UIViewController ,UIViewControllerTransitioningDelegat
     
     private var authServise: AuthServise!
     var btn: TKTransitionSubmitButton!
+    var button: TKTransitionSubmitButton!
     
   //  @IBOutlet weak var btnFromNib: TKTransitionSubmitButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         UIApplication.shared.setStatusBarStyle(.lightContent, animated: false)
-               
-               let bg = UIImageView(image: UIImage(named: "Login"))
-               bg.frame = self.view.frame
-               self.view.addSubview(bg)
+        
 
-               btn = TKTransitionSubmitButton(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width - 64, height: 44))
-               btn.center = self.view.center
-               btn.backgroundColor = .red
-               btn.frame.bottom = self.view.frame.height - 60
-               btn.setTitle("Sign in", for: UIControl.State())
-               btn.titleLabel?.font = UIFont(name: "HelveticaNeue-Light", size: 14)
-        btn.addTarget(self, action: #selector(AuthViewController.signInTouch(_:)), for: UIControl.Event.touchUpInside)
-               self.view.addSubview(btn)
+//               let bg = UIImageView(image: UIImage(named: "Login"))
+//               bg.frame = self.view.frame
+//               self.view.addSubview(bg)
 
-              // self.view.bringSubviewToFront(self.btnFromNib)
+//               btn = TKTransitionSubmitButton(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width - 64, height: 44))
+//               btn.center = self.view.center
+//              // btn.backgroundColor = #colorLiteral(red: 0.3333333333, green: 0.7254901961, blue: 0.9529411765, alpha: 1)
+//               btn.frame.bottom = self.view.frame.height - 60
+//               btn.setTitle("Sign in", for: UIControl.State())
+//               btn.titleLabel?.font = UIFont(name: "HelveticaNeue-Light", size: 14)
+//               btn.addTarget(self, action: #selector(AuthViewController.signInTouch(_:)), for: UIControl.Event.touchUpInside)
+//               self.view.addSubview(btn)
+        
+              
+        
+               button = TKTransitionSubmitButton(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width - 64, height: 44))
+               button.center = self.view.center
+               button.setTitle("Sign in", for: UIControl.State())
+               button.frame.bottom = self.view.frame.height - 60
+               button.titleLabel?.font = UIFont(name: "HelveticaNeue-Light", size: 14)
+               button.addTarget(self, action: #selector(AuthViewController.signInTouch(_:)), for: UIControl.Event.touchUpInside)
+               button.backgroundColor = #colorLiteral(red: 0.3333333333, green: 0.7254901961, blue: 0.9529411765, alpha: 1)
+             
+               self.view.addSubview(button)
+        
+//        button.translatesAutoresizingMaskIntoConstraints = false
+//        let horizontalConstraint = button.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+//        let verticalConstraint = button.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+//        let widthConstraint = button.widthAnchor.constraint(equalToConstant: 100)
+//        let heightConstraint = button.heightAnchor.constraint(equalToConstant: 100)
+//        NSLayoutConstraint.activate([horizontalConstraint, verticalConstraint, widthConstraint, heightConstraint])
+        
+        
+        
+        
+        
+        
+
         authServise = AppDelegate.shared().authServise
     }
     @IBAction func signInTouch(_ button: TKTransitionSubmitButton) {
@@ -41,14 +67,6 @@ class AuthViewController: UIViewController ,UIViewControllerTransitioningDelegat
         }
         
     }
-//    @IBAction func onTapButton(_ button: TKTransitionSubmitButton) {
-//           button.animate(1, completion: { () -> () in
-//               let secondVC = NewsFeedViewController()
-//               secondVC.transitioningDelegate = self
-//               secondVC.modalPresentationStyle = .fullScreen
-//               self.present(secondVC, animated: true, completion: nil)
-//           })
-//       }
 
        // MARK: UIViewControllerTransitioningDelegate
        func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
--- VKCleanSwift/Servise/ButtonAnimation/TransitionSubmitButton.swift
+++ VKCleanSwift/Servise/ButtonAnimation/TransitionSubmitButton.swift
@@ -3,12 +3,13 @@ import UIKit
 
 @IBDesignable
 open class TKTransitionSubmitButton : UIButton, UIViewControllerTransitioningDelegate, CAAnimationDelegate {
-    
+     
     lazy var spiner: SpinerLayer! = {
         let s = SpinerLayer(frame: self.frame)
         return s
     }()
-    
+    private var shadowLayer: CAShapeLayer!
+
     @IBInspectable open var spinnerColor: UIColor = UIColor.white {
         didSet {
             spiner.spinnerColor = spinnerColor
@@ -131,4 +132,29 @@ open class TKTransitionSubmitButton : UIButton, UIViewControllerTransitioningDel
         layer.add(expandAnim, forKey: expandAnim.keyPath)
     }
     
+    
+    
+}
+extension TKTransitionSubmitButton {
+  
+
+    open override func layoutSubviews() {
+        super.layoutSubviews()
+
+        if shadowLayer == nil {
+            shadowLayer = CAShapeLayer()
+            shadowLayer.path = UIBezierPath(roundedRect: bounds, cornerRadius: 10).cgPath
+            shadowLayer.fillColor = #colorLiteral(red: 0.3333333333, green: 0.7254901961, blue: 0.9529411765, alpha: 1)//UIColor.white.cgColor
+
+            shadowLayer.shadowColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)//UIColor.darkGray.cgColor
+            shadowLayer.shadowPath = shadowLayer.path
+            shadowLayer.shadowOffset = CGSize(width: 2.0, height: 2.0)
+            shadowLayer.shadowOpacity = 0.0
+            shadowLayer.shadowRadius = 2
+           // btn.frame.bottom = self.view.frame.height - 60
+           //btn.setTitle("Sign in", for: UIControl.State())
+            layer.insertSublayer(shadowLayer, at: 0)
+            //layer.insertSublayer(shadowLayer, below: nil) // also works
+        }
+    }
 }
